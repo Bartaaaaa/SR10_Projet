2a862fe6f146e8b2fd3a6ed24dcc44a939462624
Bartlomiej Grzadziel <bartlomiejgrzadziel2@gmail.com>
2024-04-20 20:40:00 +0200

    Ajout fonction ajouter un utilisateur

--- td3_appli/routes/users.js
+++ td3_appli/routes/users.js
@@ -10,9 +10,37 @@ router.get('/', function(req, res, next) {
 
 router.get('/userslist', function (req, res, next) {
   result=userModel.readall(function(result){
+   // userModel.creat("qzdzqd@aqzdqzdcom", "john", "Jennifer", "mdp", "tel78", "1122-12-20", "actif", function(success){
+   //   if (success) {
+   //     console.log("User inserted successfully!");
+   // } else {
+   //     console.log("Failed to insert user.");
+   // }
+ // });
   res.render('usersList', { title: 'Liste des utilisateurs', users: result });
   });
 });
 
-  
+router.post('/adduser', function (req, res, next) {
+  const nom = req.body.nom;
+  const prenom = req.body.prenom;
+  const email = req.body.mail;
+  const telephone = req.body.tel;
+  const motDePasse = req.body.mdp;
+  const dateNaissance = req.body.date;
+
+  // Appel à la fonction create de userModel avec les données du formulaire
+  result=userModel.readall(function(result){
+  res.render('usersList', { title: 'Liste des utilisateurs', users: result });
+  });
+  if (email!==null){
+  userModel.creat(email, prenom, nom, motDePasse, telephone, dateNaissance, "actif", function(success){
+     if (success) {
+       console.log("User inserted successfully!");
+   } else {
+       console.log("Failed to insert user.");
+   }
+  });}
+});
+
 module.exports = router;
