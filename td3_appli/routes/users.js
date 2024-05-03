1a543a1c9c777376e1ee81b474b91457bd2398f3
Bartlomiej Grzadziel <bartlomiejgrzadziel2@gmail.com>
2024-05-03 12:25:37 +0200

    Work in progress : connexion & inscription page bientot finalisée, gestion des inscriptions fonctionnelles, reste à lier la conenxion a la base du site

--- td3_appli/routes/users.js
+++ td3_appli/routes/users.js
@@ -17,41 +17,31 @@ router.get('/userslist', function (req, res, next) {
 
 //Ajouter un utilisateur 
 //Ajouter un utilisateur 
-router.post('/adduser', function (req, res, next) {
-  const nom = req.body.nom;
-  const prenom = req.body.prenom;
-  const email = req.body.mail;
-  const telephone = req.body.tel;
-  const motDePasse = req.body.mdp;
-  //const dateNaissance = req.body.date;
-
+router.post('/adduser', function (req, res) {
+  const { nom, prenom, mail: email, tel: telephone, mdp: motDePasse } = req.body;
   const currentDate = new Date();
-// Extraire l'année, le mois et le jour de la date actuelle
   const year = currentDate.getFullYear();
   const month = String(currentDate.getMonth() + 1).padStart(2, '0');
   const day = String(currentDate.getDate()).padStart(2, '0');
   const formattedDate = `${year}-${month}-${day}`;
 
-  // Appel à la fonction create de userModel avec les données du formulaire
-  result = userModel.readall(function(result) {
-    res.render('usersList', { title: 'Liste des utilisateurs', users: result });
+  userModel.areValid(email, function(isValid) {
+      if (isValid) {
+          userModel.creat(email, prenom, nom, motDePasse, telephone, formattedDate, "actif", function(success) {
+              if (success) {
+                  console.log("User inserted successfully!");
+                  res.json({ message: "User inserted successfully" });
+              } else {
+                  console.log("Failed to insert user.");
+                  res.status(500).json({ error: "Failed to insert user" });
+              }
+          });
+      } else {
+          console.log("Email is not valid.");
+          res.status(400).json({ error: "Email is not valid" });
+      }
+  });
 });
 
-if (email !== null) {
-    userModel.areValid(email, function(isValid) {
-        if (isValid) {
-            userModel.creat(email, prenom, nom, motDePasse, telephone, formattedDate, "actif", function(success) {
-                if (success) {
-                    console.log("User inserted successfully!");
-                } else {
-                    console.log("Failed to insert user.");
-                }
-            });
-        } else {
-            console.log("Email is not valid.");
-        }
-    });
-}
-});
 
 module.exports = router;
