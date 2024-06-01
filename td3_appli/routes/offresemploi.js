93c0db8bed6376af264cac4123491f662cc8024f
Bartlomiej Grzadziel <bartlomiejgrzadziel2@gmail.com>
2024-06-01 15:55:29 +0200

    test

--- td3_appli/routes/offresemploi.js
+++ td3_appli/routes/offresemploi.js
@@ -19,7 +19,8 @@ router.get('/offresemploilist', function (req, res, next) {
 const upload = require('../config/multer-config'); // Import multer configuration
 
 var Candidaturemodel= require('../model/Candidature')
-
+//upload.array('fileUpload', 10) : C'est un middleware multer qui gère le téléchargement de fichiers. Il permet de télécharger jusqu'à 10 fichiers à la fois, provenant du champ de formulaire fileUpload.
+//upload.array('fileUpload', 10) : Ce middleware traite les fichiers envoyés sous le champ fileUpload et les enregistre dans le répertoire uploads selon la configuration définie.
 router.post('/addCandidature', upload.array('fileUpload', 10), function(req, res, next) {
   if (req.session.userid) {
     Candidaturemodel.read(req.session.userid, function(result) {
@@ -32,9 +33,10 @@ router.post('/addCandidature', upload.array('fileUpload', 10), function(req, res
       const offreEmploi = req.body.offreEmploi;
       const candidat = req.session.userid;
       const date = req.body.date;
+      //const pieces = req.files.map(file => file.path); : Cette ligne crée un tableau des chemins des fichiers téléchargés. req.files est rempli par multer et contient des informations sur chaque fichier téléchargé.
       const pieces = req.files.map(file => file.path); // Array of file paths
       const etat = req.body.etat;
-
+//pieces.join(',') : Convertit le tableau des chemins des fichiers en une chaîne de caractères séparée par des virgules.
       Candidaturemodel.create(offreEmploi, candidat, date, pieces.join(','), etat, function(success, err) {
         if (err) {
           return res.status(500).json({ error: "Erreur lors de l'enregistrement de la candidature" });
